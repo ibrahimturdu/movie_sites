07b58120f80bf0792aed72b641a792e568637a78
ibrahimturdu <ibrahimmturdu@gmail.com>
2023-05-30 22:04:00 +0300

    solved erorr

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const arrows = document.querySelectorAll(".arrow");
 const movieLists = document.querySelectorAll(".movie-list");
-
 // arrows.forEach((arrow, i) => {
 //     arrow.addEventListener("click", function () {
 //         movieLists[i].style.transform = `translateX(${
 //             movieLists[i].computedStyleMap().get("transform")[0].x.value-300}px)`;
 //     });
 // });
-
-
 arrows.forEach((arrow, i) => {
+    let clickCounter = 0;
+    const imageItem = movieLists[i].querySelectorAll("img").length;
     arrow.addEventListener('click', function () {
-      movieLists[i].style.transform = `translateX(${
-        movieLists[i].computedStyleMap().get('transform')[0].x.value - 300
-      }px)`;
+        clickCounter++;
+        if (imageItem - (4 + clickCounter) >= 0) {
+            movieLists[i].style.transform = `translateX(${movieLists[i].computedStyleMap().get('transform')[0].x.value - 300
+                }px)`;
+        }
+        else {
+            movieLists[i].style.transform("translateX(0)")
+        }
     });
-  });
+});
